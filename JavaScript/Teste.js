086f32e0942049e7fade52bcd0a8ed17ace95f40
Paulo Martins <martinspaullo>
2023-09-30 00:59:53 -0300

    if else

--- JavaScript/Teste.js
+++ JavaScript/Teste.js
@@ -1,14 +1,13 @@
 function numeroPositivo(num){
-    let resultado;
+    var resultado;
     if (num > 0){
         resultado = "Número Positivo";
     } else{
         resultado = "Número Negativo";
     }
-
+    
+    console.log(resultado);
     return resultado;
-
 }
 
-numeroPositivo(2);
-console.log(resultado);
+numeroPositivo(-2);
