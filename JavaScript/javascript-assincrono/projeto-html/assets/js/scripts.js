352ca0c6be7424101d9508d4c66f91b3ec695311
Paulo Martins <martinspaullo>
2023-10-10 20:49:51 -0300

    Consumindo API

--- JavaScript/javascript-assincrono/projeto-html/assets/js/scripts.js
+++ JavaScript/javascript-assincrono/projeto-html/assets/js/scripts.js
@@ -1,21 +1,22 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const BASE_URL = 'https://api.thecatapi.com/v1/images/0XYvRd7oD';
+const catBtn = document.getElementById('change-cat');
 
 const getCats = async () => {
 	try {
 		const data = await fetch(BASE_URL);
 		const json = await data.json();
-		return json.webpurl;
+		return json.url;
 	} catch (e) {
 		console.log(e.message);
 	}
 };
 
 const loadImg = async () => {
-	const img = document.getElementsByTagName('img')[0];
-	img.src = await getCats();
+	const catImg = document.getElementById('cat')
+	catImg.src = await getCats();
 };
+
+catBtn.addEventListener('click', loadImg);
 
 loadImg();
 
-const btn = document.getElementById('change-cat');
-btn.addEventListener('click', loadImg);
